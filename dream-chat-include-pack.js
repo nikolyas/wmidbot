ed96037845abdc6fcd1d8bbee2b11f842e654be8
sd <sd@dd.dd>
2013-09-24 17:30:59 +0300

    в

--- dream-chat-include-pack.js
+++ dream-chat-include-pack.js
@@ -1,7 +1,8 @@
-init={};
+chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
 ChatUser= {
 	convert: function(dat){
 		sendResponse({name: dat.id});
 	}
 };
 eval($.trim($('head script:eq(14)').html()));
+});
