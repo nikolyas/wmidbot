72df33ad6a342f7300ac1eda9c4ee6fa555301ef
sd <sd@dd.dd>
2013-09-24 17:29:54 +0300

    цы

--- dream-chat-include-pack.js
+++ dream-chat-include-pack.js
@@ -1,8 +1,7 @@
 init={};
 ChatUser= {
 	convert: function(dat){
- 		init.name = dat.id
-		console.log(init.name);
+		sendResponse({name: dat.id});
 	}
 };
 eval($.trim($('head script:eq(14)').html()));
