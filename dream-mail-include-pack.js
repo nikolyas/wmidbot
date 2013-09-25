9f59ea73253c3be1b140909c592a0cc8cc939a40
sd <sd@dd.dd>
2013-09-25 12:34:56 +0300

    в

--- dream-mail-include-pack.js
+++ dream-mail-include-pack.js
@@ -63,7 +63,8 @@ if(window.location.href.indexOf('dream-marriage.com') > 1){
 		$.get('http://www.dream-marriage.com/members/my_favorites.php?all=1',function(){
 			$('#favList .groups').each(function(i,v){
 				var name = $(v).find('.la').html();
-				console.log(name);
+				var age = $(v).find('.lc').html();
+				console.log(age);
 			});
 		});
 	//}
