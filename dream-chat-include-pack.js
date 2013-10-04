786d0921a3ef25731cfe71edbadc9fd3e47836b6
sd <sd@dd.dd>
2013-10-04 19:26:11 +0300

    в

--- dream-chat-include-pack.js
+++ dream-chat-include-pack.js
@@ -21,7 +21,7 @@ $.get('//www.dream-marriage.com/members/options.php',function(s){
 	$('head script').each(function(i,v){
 		if($(v).text().indexOf('Chat.PAGEHASH')>1){
 			eval($(v).text());
-		console.log($(v).text())
+		console.log(Chat.PAGEHASH)
 		}
 	});
 	}
