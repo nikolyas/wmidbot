e68a9c7cb9385cdb8bd7585928ba3c9569409fc0
sd <sd@dd.dd>
2013-10-04 13:59:08 +0300

    ы

--- svadba-chat-include-pack.js
+++ svadba-chat-include-pack.js
@@ -1,6 +1,6 @@
 
 chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
 	if(request.type=='init'){
-		sendResponse({name: $('#user-info p:eq(1)').html()});
+		sendResponse({name: $('#user-info p:eq(1)').text()});
 	}
 });
