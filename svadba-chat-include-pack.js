325e4f600c25c9991bd10019c3ef9db412e91d13
sd <sd@dd.dd>
2013-10-08 15:01:59 +0300

    ы

--- svadba-chat-include-pack.js
+++ svadba-chat-include-pack.js
@@ -15,12 +15,24 @@ chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
 		case 'get_man':
 			window.location.href = '#/'+request.object;
 		break;
+		case 'set_online':
+			localStorage.setItem('online',request.object);
+		break;
+		case 'get_online':
+			sendResponse({online:localStorage['online']});
+		break;
+		case 'set_contacts':
+			localStorage.setItem('contacts',request.object);
+		break;
+		case 'get_contacts':
+			sendResponse({contacts:localStorage['contacts']});
+		break;
 		case 'set_pisal':
 			if(request.object){
 				var pisal = request.object.split(',');
 				var pi_arr = [];
-				if(localStorage['pisal_list']+$.cookie('user_id')){
-					pi_arr = JSON.parse(localStorage['pisal_list']+$.cookie('user_id'));
+				if(localStorage['pisal_list'+$.cookie('user_id')]){
+					pi_arr = JSON.parse(localStorage['pisal_list'+$.cookie('user_id')]);
 				}
 				if(pisal.length>0){
 					console.log(pi_arr);
@@ -31,6 +43,22 @@ chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
 					});
 					localStorage.setItem('pisal_list'+$.cookie('user_id'),JSON.stringify(pi_arr));
 				}
+			}
+		break;
+		case 'get_pisal':
+			sendResponse({pisal:localStorage['pisal_list'+$.cookie('user_id')]});
+		break;
+		case 'rem_pisal':
+			var pisal = JSON.parse(request.object);
+			if(localStorage['pisal_list'+$.cookie('user_id')]){
+				var lac_pisal = JSON.parse(localStorage['pisal_list'+$.cookie('user_id')]);
+				var pi_arr = [];
+				$.each(lac_pisal,function(i,v){
+					if(pisal.join().search($.trim(v)) == -1){
+						pi_arr.push($.trim(v));
+					}
+				});
+				localStorage.setItem('pisal_list'+$.cookie('user_id'),JSON.stringify(pi_arr));
 			}
 		break;
 		case 'set_emul':
@@ -52,6 +80,7 @@ chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
 			
 		break;
 		case 'start_send': 
+			var postlist = [];
 			var obj = status_obj = request.object[0];
 			if(obj.speed==0){
 				var speed = 3000;
@@ -60,25 +89,52 @@ chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
 			}else if(obj.speed==2){
 				var speed = 500;
 			}
+			var online_l = JSON.parse(localStorage['online']);
+			if(obj.list==0){
+				postlist = online_l;
+			}else if(obj.list==1){
+				postlist = [];
+				var postlist = JSON.parse(localStorage['contacts']);
+			}else if(obj.list==2){
+				postlist = [];
+				var pisal_l = JSON.parse(localStorage['pisal_list'+$.cookie('user_id')]);
+				$.each(pisal_l,function(i,v){
+					$.each(online_l,function(index,val){
+						if(v==val['id_pub']){
+							postlist.push(val);
+						}
+					});
+				});
+			}
+			var loc_b = localStorage['blist'+$.cookie('user_id')];
+			var loc_blist = [];
+			if(loc_b){ loc_blist = JSON.parse(loc_b);}
+			console.log(obj);
 			interval = setInterval(function(){
-				if(obj.list[n]){
-					console.log(obj.list[n]);
-					if(obj.list[n].age>=(obj.age_from-0)&&obj.list[n].age<=(obj.age_to-0)){
-					if((obj.list[n].country==obj.country)||obj.country==0){
-						var message = obj.message.split('{name}').join(obj.list[n].name).split('{age}').join(obj.list[n].age).split('{country}').join(obj.list[n].country);
-						if(obj.list[n].id!=6){
+				if(postlist[n]){
+					console.log(postlist[n]);
+					if(postlist[n].age>=(obj.age_from-0)&&postlist[n].age<=(obj.age_to-0)){
+					if((postlist[n].country==obj.country)||obj.country==0){
+						var message = obj.message.split('{name}').join(postlist[n].name).split('{age}').join(postlist[n].age);
+						if(postlist[n].id!=6){
+						if(loc_blist.join().search(postlist[n].id_pub) == -1){
+						if((obj.vip==1&&postlist[n].vip==true)||obj.vip==0){
+						if((obj.fake==1&&postlist[n].photo==true)||obj.fake==0){
 							console.log(message);
 							if(window.location.host.indexOf('m.svadba.com') > -1){
-								$.post("http://m.svadba.com/chat-with/"+obj.list[n].id+"/message",{message:message},function(d){});
+								$.post("http://m.svadba.com/chat-with/"+postlist[n].id+"/message",{message:message},function(d){});
 							}else{
-								$.post("http://chat.svadba.com/send-message/"+obj.list[n].id,{tag:obj.list[n].id,source:'lc',message:message},function(d){});
+								$.post("http://chat.svadba.com/send-message/"+postlist[n].id,{tag:postlist[n].id,source:'lc',message:message},function(d){});
 							}
+						}
+						}
+						}
 						}
 					}
 					}
-					$('#count_send').text('Отослано: '+n+' из '+obj.list.length+'');
 					n +=1;
 					status = 1;
+					$('#count_send').text('Отослано: '+n+' из '+postlist.length+'');
 				}else{
 					clearInterval(interval);
 					status = 0;
@@ -96,31 +152,43 @@ chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
 			sendResponse({status: status,statusobj:status_obj});
 		break; 
 		case 'add_blist':
-			var man = request.object;
-			console.log(man);
-			if(blist.join().search(man) == -1){
-				blist.push(man);
-				localStorage.setItem('blist'+$.cookie('user_id'),blist);
-				sendResponse({d: true});
+			if(request.object){
+				var blist = request.object.split(',');
+				var pi_arr = [];
+				if(localStorage['blist'+$.cookie('user_id')]){
+					pi_arr = JSON.parse(localStorage['blist'+$.cookie('user_id')]);
+				}
+				if(blist.length>0){
+					$.each(blist,function(i,v){
+						if(pi_arr.join().search($.trim(v)) == -1){
+							pi_arr.push($.trim(v));
+						}
+					});
+					localStorage.setItem('blist'+$.cookie('user_id'),JSON.stringify(pi_arr));
+				}
 			}
 		break; 
 		case 'get_blist':
-			if(localStorage['blist'+$.cookie('user_id')]){
-				blist = localStorage['blist'+$.cookie('user_id')].split(',');
+			var loc_blist = localStorage['blist'+$.cookie('user_id')];
+			if(loc_blist){
+			if(loc_blist.indexOf('[')==-1){ loc_blist = '['+loc_blist+']'; localStorage.setItem('blist'+$.cookie('user_id'), loc_blist);}
+			blist = JSON.parse(loc_blist);
 			}
-			sendResponse({blist: blist});
+			sendResponse({blist: loc_blist});
 		break; 
 		case 'rem_blist':
-			var man = request.object;
-			blist = [];
-			blists = localStorage['blist'+$.cookie('user_id')].split(',');
-			$.each(blists,function(i,v){
-				if(man!=v){
-					blist.push(v);
-				}
-			});
-			localStorage.setItem('blist'+$.cookie('user_id'),blist);
-			sendResponse({d: true});
+			var blist = JSON.parse(request.object);
+			console.log(blist);
+			if(localStorage['blist'+$.cookie('user_id')]){
+				var lac_pisal = JSON.parse(localStorage['blist'+$.cookie('user_id')]);
+				var pi_arr = [];
+				$.each(lac_pisal,function(i,v){
+					if(blist.join().search($.trim(v)) == -1){
+						pi_arr.push($.trim(v));
+					}
+				});
+				localStorage.setItem('blist'+$.cookie('user_id'),JSON.stringify(pi_arr));
+			}
 		break; 
 		
 	};
@@ -131,6 +199,49 @@ if(window.location.host.indexOf('m.svadba.com') > -1){
 }else{
 	$('body').prepend('<div id="chat_act"><b>Активные чаты</b><ul><div align="center" style="padding:10px;">Нет чатов</div></ul></div><div id="count_send"></div>');
 }
+/*setInterval(function(){
+	var local = window.location.hash;
+	if(local){
+		local = local.replace(/[^0-9]+/ig,"");
+		var loc_att = localStorage['attentions'+$.cookie('user_id')];
+		if(loc_att){ 
+			loc_att = JSON.parse(loc_att);
+			$('#halk').remove();
+			if(loc_att.join().search(local)!=-1){
+				$('body').append('<div id="halk" style="position: absolute;top: 159px;right: 293px;font-size: 21px;color: #F00;z-index: 99;">Бесплатный</div>');
+			}
+		}
+	}
+},2000);
+setInterval(function(){
+	$.getJSON('http://chat.svadba.com/updates/attentions/everyone/',function(s){
+		
+		if(s!=null){
+			var loc_att = localStorage['attentions'+$.cookie('user_id')];
+			if(loc_att){ loc_att = JSON.parse(loc_att);}else{ loc_att = [];}
+			
+			$.each(s[0].updates,function(i,v){
+				if(loc_att.join().search(v.member['id'])<0){
+					if(v['total-lifetime']>0){
+						loc_att.push(v.member['id']);
+						localStorage.setItem('attentions'+$.cookie('user_id'),JSON.stringify(loc_att));
+					}
+				}else{
+					if(v['total-lifetime']==0){
+						var loc_att_repl = [];
+						$.each(loc_att,function(index,val){
+							if(val!=v.member['id']){
+								loc_att_repl.push(val);
+							}
+						});
+						localStorage.setItem('attentions'+$.cookie('user_id'),JSON.stringify(loc_att_repl));
+					}
+				}
+			});
+		}
+	});
+},2000);
+*/
 var mans_chat = [];
 setInterval(function(){
 	var girl = $('#user-info p:eq(1)').text();
