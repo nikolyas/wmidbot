b7ce21d68c517362746d49a1914685f521eef706
root <root@srv2.sim-networks.net>
2013-11-20 15:08:46 +0100

    admin commit

--- svadba-chat-include-pack.js
+++ svadba-chat-include-pack.js
@@ -8,6 +8,7 @@ var online = [];
 var stor = 0;
 
 $('head').append('<style>#chat_act .message {height:11px!important;}</style>');
+$('#translate').show();
 $('#translate input[type="submit"]').css({ width: '112px', height: '27px', background: '#26ade4', textIndent: '0', lineHeight: 'auto', marginRight: '20px', fontWeight: 'bold', color: '#fff'});
 $('#translate .popup').hide();
 
