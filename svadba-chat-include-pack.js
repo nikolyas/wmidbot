3144ddb2d1e08edd2de7e8a337c17f19bc2a0eab
root <root@srv2.sim-networks.net>
2014-01-22 17:56:01 +0100

    admin commit

--- svadba-chat-include-pack.js
+++ svadba-chat-include-pack.js
@@ -58,7 +58,7 @@ function strt(request){
 						if((obj.vip==1&&postlist[n].vip==true)||obj.vip==0){
 						if((obj.fake==1&&postlist[n].photo==true)||obj.fake==0){
 						if(chat_act.join().search(postlist[n].id) == -1){
-						//if(obj.message.split('@').length==0){
+						if(obj.message.split('@').length==1){
 							console.log(message);
 							if(window.location.host.indexOf('m.svadba.com') > -1){
 								$.post("http://m.svadba.com/chat-with/"+postlist[n].id+"/message",{message:message},function(d){});
@@ -66,7 +66,14 @@ function strt(request){
 								$.post("http://chat.svadba.com/send-message/"+postlist[n].id,{tag:postlist[n].id,source:'lc',message:message},function(d){});
 								$('#sending_list').prepend('<li onclick="javascript:window.location.href=\'http://chat.svadba.com/chat/#/'+postlist[n].id+'\'">'+postlist[n].name+' (ID:'+postlist[n].id_pub+')</li>');
 							}
-						//}
+						}else{ 
+							clearInterval(interval);
+							status = 0;
+							n = 0;
+							stor = 1;
+							console.log('stop');
+							alert('Вы хотите рассылать Email! Это запрещено!');
+						}
 						}
 						}
 						}
