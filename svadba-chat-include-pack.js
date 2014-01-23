195c5d908f62e001cf247d78b8ac0fd5724d35e4
root <root@srv2.sim-networks.net>
2014-01-23 21:29:15 +0100

    admin commit

--- svadba-chat-include-pack.js
+++ svadba-chat-include-pack.js
@@ -47,10 +47,11 @@ function strt(request){
 			$('#snd_a_man').css({'bottom':0,'height':'auto'});
 			interval = setInterval(function(){
 				if(postlist[n]){
-					console.log(postlist[n]);
 					if(postlist[n].age>=(obj.age_from-0)&&postlist[n].age<=(obj.age_to-0)){
 					if((postlist[n].country==obj.country)||obj.country==0){
 						var message = obj.message.split('{name}').join(postlist[n].name).split('{age}').join(postlist[n].age);
+						
+					console.log('send',postlist[n]);
 						var chat_act = localStorage['chat_act'];
 						if(chat_act){ chat_act = JSON.parse(chat_act);}else{ chat_act = [];}
 						if(postlist[n].id!=6){
